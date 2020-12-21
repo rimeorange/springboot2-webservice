5a088875ec5d504e41a1b5f824681723affaf9e8
rimeorange <rimeorange83@gmail.com>
2020-12-21 11:36:11 +0900

    스프링 시큐리티 - 어노테이션 기반으로 개선.

--- src/main/java/com/rimeorange/book/springboot/web/IndexController.java
+++ src/main/java/com/rimeorange/book/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.rimeorange.book.springboot.web;
 
+import com.rimeorange.book.springboot.config.auth.LoginUser;
 import com.rimeorange.book.springboot.config.auth.dto.SessionUser;
 import com.rimeorange.book.springboot.domain.user.User;
 import com.rimeorange.book.springboot.service.PostsService;
 import com.rimeorange.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,13 +19,11 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
